bad176476d2d8e2d8b753fa1c85b3533b8f2ecd5
Eyal <eyaldelarea@gmail.com>
2018-12-01 15:06:22 +0200

    adding pictures and progress bar while waiting

--- app/src/main/java/com/example/shakeddesk/light/MainActivity.java
+++ app/src/main/java/com/example/shakeddesk/light/MainActivity.java
@@ -4,34 +4,33 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-    private static final String SP_IP = "ip";
-    private static final String SP_PORT = "port";
 
     public static final String COMMAND_TOGGLE = "TOGGLE";
     public static final String COMMAND_GET_STATE = "STATE";
-
     public static final String RESPONSE_STATE_ON = "1";
     public static final String RESPONSE_STATE_OFF = "0";
     public static final String RESPONSE_TOGGLE = "TOGGLED";
-
+    private static final String SP_IP = "ip";
+    private static final String SP_PORT = "port";
+    public Button goBtn, btnState;
     private EditText etIP, etPort;
-    private Button goBtn;
     private ImageView imageState;
+    private ProgressBar progressBar;
     private boolean changedIP = false;
     private boolean changedPort = false;
 
@@ -45,11 +44,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         goBtn = findViewById(R.id.btnGo);
         goBtn.setOnClickListener(this);
         imageState = findViewById(R.id.imageState);
+        progressBar = findViewById(R.id.progressBar);
+        btnState = findViewById(R.id.btnState);
+        btnState.setOnClickListener(this);
 
-        findViewById(R.id.btnState).setOnClickListener(this);
+        progressBar.setVisibility(View.GONE);
 
 
         loadData(); // loads initial data
+
+        getLightState();
 
 
         findViewById(R.id.defaultButton).setOnClickListener(new View.OnClickListener() {
@@ -78,6 +82,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
 
+    }
+
+    private void getLightState() {
+
+        setProgressBarON();
+
+
+        final SendCommand2 sm2 = new SendCommand2(this,
+                etIP.getText().toString(), Integer.valueOf(etPort.getText().toString()),
+                SendCommand2.COMMAND_GET_STATE, new Runnable() {
+            @Override
+            public void run() {
+
+
+            }
+        }, new SendCommand2.ServerAnswer() {
+            @Override
+            public void callback(String answer) {
+
+
+                if (answer.equals(SendCommand2.RESPONSE_STATE_ON)) {
+
+                    imageState.setImageResource(R.drawable.lighton);
+                    Toast.makeText(MainActivity.this, "Light is ON ! ", Toast.LENGTH_SHORT).show();
+
+                } else if (answer.equals(SendCommand2.RESPONSE_STATE_OFF)) {
+
+                    imageState.setImageResource(R.drawable.lightoff);
+                    Toast.makeText(MainActivity.this, "Light is OFF ! ", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    imageState.setImageResource(R.drawable.erorrimage);
+                    Toast.makeText(MainActivity.this, "getLightStateError " + answer
+                            , Toast.LENGTH_SHORT).show();
+                    Log.e("GET-LIGHT-STATE-ERROR ", answer);
+                }
+
+                setProgressBarOFF();
+            }
+
+        });
+
+        sm2.start();
+
+    }
+
+    private void doToggle() {
+
+
+        final SendCommand2 sm2 = new SendCommand2(this,
+                etIP.getText().toString(), Integer.valueOf(etPort.getText().toString()),
+                SendCommand2.COMMAND_TOGGLE, new Runnable() {
+            @Override
+            public void run() {
+
+              setProgressBarON();
+
+            }
+        }, new SendCommand2.ServerAnswer() {
+
+
+            @Override
+            public void callback(String answer) {
+
+
+                if (answer.equals(SendCommand2.RESPONSE_TOGGLE)) {
+
+                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+
+                    // change view error .
+                    Toast.makeText(MainActivity.this, "doToggleERROR " + answer
+                            , Toast.LENGTH_SHORT).show();
+                    Log.e("DO-TOGGLE-ERROR ", answer);
+                }
+                getLightState();
+                setProgressBarOFF();
+            }
+        });
+
+        sm2.start();
+    }
+
+    private void setProgressBarON(){
+
+        progressBar.setVisibility(View.VISIBLE);
+        imageState.setVisibility(View.GONE);
+        btnState.setEnabled(false);
+        goBtn.setEnabled(false);
+        Toast.makeText(this, "Please Wait", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void setProgressBarOFF(){
+        progressBar.setVisibility(View.GONE);
+        imageState.setVisibility(View.VISIBLE);
+        btnState.setEnabled(true);
+        goBtn.setEnabled(true);
+        Toast.makeText(this, "Finished Waiting", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -111,15 +217,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.btnGo) {
             if (permission()) {
                 saveChanges(); // each time you click "go" I save the current ip and port (smartly)
-                go(COMMAND_TOGGLE);
+                doToggle();
             } else {
                 Toast.makeText(this, "Don't have permission to do so", Toast.LENGTH_SHORT).show();
             }
         } else if (view.getId() == R.id.btnState) {
-            go(COMMAND_GET_STATE);
+            getLightState();
         }
     }
-
+/*
     private void go(String command) {
         try {
             Thread t = new SendCommand(this, etIP.getText().toString(), Integer.valueOf(etPort.getText().toString()), goBtn, command);
@@ -127,8 +233,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-    }
-
+    }*/
 
 
     private void saveChanges() {
